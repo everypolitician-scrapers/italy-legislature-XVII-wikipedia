8a3674a98a0dc06cf7c52c5cf28668222bdb4086
Tony Bowden <tony@tmtm.com>
2015-09-23 11:53:09 +0100

    initial scraper

---
+++ scraper.rb
@@ -0,0 +1,39 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.xpath('//h2[span[@id="Modifiche_Intervenute"]]/following-sibling::*').map(&:remove)
+
+  h2 = noko.xpath('//h2[span[@id="Gruppi_Parlamentari"]]')
+  h3s = h2.xpath('following-sibling::h2 | following-sibling::h3').slice_before { |e| e.name != 'h3' }.first
+  h3s.first.xpath('following-sibling::ul/li').each do |li|
+    who = li.css('a').first
+    data = {
+      name: who.text.tidy,
+      wikiname: who.attr('class') == 'new' ? nil : who.attr('title'),
+      faction: li.xpath('preceding::h3').last.css('span.mw-headline').text,
+      source: url,
+    }
+    ScraperWiki.save_sqlite([:name, :faction], data)
+  end
+end
+
+scrape_list('https://it.wikipedia.org/wiki/Deputati_della_XVII_Legislatura_della_Repubblica_Italiana')
